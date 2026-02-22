22c0b08a0bad360c0c71b11de6783a29bc985bee
Cookie <cookienumerique@gmail.com>
2026-02-22 11:09:53 +0100

    feat(subscription): add founder base plan

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ docker exec -ti php_selen php bin/console doctrine:schema:update --dump-sql
 
 ## Installation
 - installer le fichier google-service-account.json
+
+
+## clean cache
+docker exec -ti php_selen composer dump-autoload && php bin/console cache:clear
--- src/Enum/Subscription/SubscriptionBasePlanId.php
+++ src/Enum/Subscription/SubscriptionBasePlanId.php
@@ -6,4 +6,6 @@ enum SubscriptionBasePlanId: string
 {
   case SELEN_PREMIUM_MONTHLY = 'selen-premium-monthly';
   case SELEN_PREMIUM_YEARLY = 'selen-premium-yearly';
+  case SELEN_PREMIUM_MONTHLY_FOUNDER = 'selen-premium-monthly-founder';
+  case SELEN_PREMIUM_YEARLY_FOUNDER = 'selen-premium-yearly-founder';
 }
